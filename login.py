baa304fa60b2371e45eadb1bce378ddb94d675d8
Inas Fawzi <inasfawzi24@gmail.com>
2023-02-27 16:00:48 -0500

    Furthur editing login functionality

--- login.py
+++ login.py
@@ -1,20 +1,22 @@
+#
+import sqlite3
 #importing Flask and other modules
 from flask import Flask, request, render_template
 
 #Flask constructor
-app = Flask(__name__)
+loginFunctionality = Flask(__name__)
 
 # A decorator used to tell the application which URL is associated function
-@app.route('/login.html', methods =["GET", "POST"])
+@loginFunctionality.route('/login.html', methods =["GET", "POST"])
 def login():
-    if request.method == "GET":
-        return render_template("form.html")
-    else: #if the request method is POST
+    if request.method == "POST":
        # getting input with name = fname in HTML form
        email = request.form.get("email")
        # getting input with name = lname in HTML form
        password = request.form.get("password")
+       print(email + password)
        return "Your email is "+ email + "Your password is "+ password
+    return render_template('login.html', boolean=True)
 
 if(__name__ == '__main__'):
-    app.run(debug=True)
+    loginFunctionality.run(debug=True)

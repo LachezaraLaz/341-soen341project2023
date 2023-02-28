1f6964e0683566b9a9d40dc69603326513bb95c1
Inas Fawzi <inasfawzi24@gmail.com>
2023-02-27 20:01:07 -0500

    loginHTML.html was modified so that I could retrive the form information from it with oython. form method POST was added, as well as name attributed for certai form inputs. login.py was changed to further implement the login functionality although it is unfinished.

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-#
+#importing the sqlite3 module to handle database
 import sqlite3
 #importing Flask and other modules
 from flask import Flask, request, render_template
@@ -7,16 +7,67 @@ from flask import Flask, request, render_template
 loginFunctionality = Flask(__name__)
 
 # A decorator used to tell the application which URL is associated function
-@loginFunctionality.route('/login.html', methods =["GET", "POST"])
+@loginFunctionality.route('/loginHTML.html', methods =['GET', 'POST'])
 def login():
-    if request.method == "POST":
+    if request.method == 'POST':
        # getting input with name = fname in HTML form
        email = request.form.get("email")
        # getting input with name = lname in HTML form
        password = request.form.get("password")
-       print(email + password)
-       return "Your email is "+ email + "Your password is "+ password
-    return render_template('login.html', boolean=True)
+       if (email == None):
+          print("email is null")
+       if (password == None):
+          print("password is null")
+
+       else:
+          print(email + password)
+
+          #connection to the database module
+          conn = sqlite3.connect("data.db")
+          # allow for SQL commands to be run
+          c = conn.cursor()
+          row = c.execute("SELECT userKey, email, password, userType from LoginInfo").fetchall()
+
+          emailMatch = False
+          passwordMatch = False
+          userID = None
+          userType = None
+          rowCounter = 0
+          trackRow = None
+
+          for x in row:
+            columnCounter = 0
+            rowCounter += 1
+            for y in x:
+               columnCounter += 1
+               print(y, end=' ')
+               if (trackRow == None and columnCounter == 1):
+                  userID = y
+               if (columnCounter == 2):
+                  if (email == y):
+                     print("matching email!")
+                     emailMatch = True
+                     trackRow = rowCounter
+               if (columnCounter == 3):
+                  if (password == y):
+                     print("matching password!")
+                     passwordMatch = True
+               if (rowCounter == trackRow and emailMatch == True and passwordMatch == True and columnCounter == 4):
+                  userType = y
+            print()
+
+          conn.commit()
+          c.close()
+          
+          if (passwordMatch == True and emailMatch == True):
+             print("credentials valid, proceeding with login "+ str(userID) + " " + email + " " + password + " " + userType)
+             if (userType == "student"):
+                print("redirect to student profle")
+             if (userType == "employer"):
+                print("redirect to employer dashboard")
+          else:
+             print("incorrect login crendentials")
+    return render_template('loginHTML.html', boolean=True)
 
 if(__name__ == '__main__'):
     loginFunctionality.run(debug=True)

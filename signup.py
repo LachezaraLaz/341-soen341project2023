629564026d31669850011f378e54199dd93ed7a2
Suha <88583460+suha-ab@users.noreply.github.com>
2023-03-01 02:07:34 -0500

    implement dynamic job postings and loading of jobs and verifies no such user in database before signup

--- signup.py
+++ signup.py
@@ -24,23 +24,57 @@ def signup():
         # connection to the database module
         conn = sqlite3.connect("data.db")
         c = conn.cursor()
+
+        # Verify that the user doesn't already exist
+        checkUser = "SELECT email FROM LoginInfo WHERE email =\'" + str(email) + "\'"
+        userExists = c.execute(checkUser).fetchone()
+        if(userExists != None):
+            return "user already exists"
+
+        # If user doesn't exist, add them to the database
         lastKey = c.execute("SELECT userKey FROM LoginInfo ORDER BY userKey DESC LIMIT 1").fetchone()[0]
         print("Key inside:",lastKey)
 
         # fetch row and store new record
         row = c.execute("SELECT userKey, email, password FROM LoginInfo").fetchall()
         lastKey = c.execute("SELECT userKey FROM LoginInfo ORDER BY userKey DESC").fetchone()[0]
-        #newRecord = "INSERT INTO LoginInfo VALUES ("+str(lastKey+1)+","+str(email[0:email.index("@")])+","+str(password)+", 'employer')"
-        newRecord = "INSERT INTO LoginInfo VALUES (" + str(lastKey+1) + ",\'" + str(email) + "\',\'" + str(password) + "\',\'" + str(userType) + "\')"
+        newUser = "INSERT INTO LoginInfo VALUES (" + str(lastKey+1) + ",\'" + str(email) + "\',\'" + str(password) + "\',\'" + str(userType) + "\')"
 
-        #newRecord = "INSERT INTO LoginInfo VALUES ("+ str(lastKey+1)+", " + str("success test") + ", " + str(password) + ", 'testType')"
-        row = c.execute(newRecord)
+        # Create new user profile
+        if(userType == "Student"):
+            lastProfileKey = c.execute("SELECT profileKey FROM UserProfiles ORDER BY profileKey DESC LIMIT 1").fetchone()[0]
+            newProfile = "INSERT INTO UserProfiles (profileKey, userID) VALUES (" + str(lastProfileKey+1) +"," + str(lastKey+1) + ")"
+            c.execute(newProfile)
+        c.execute(newUser)
         conn.commit()
         c.close()
-        return "sucess"
+        return render_template('loginHTML.html')
 
+@app.route('/jobPostings',methods = ['POST','GET'])
+def jobPostings():
+    if request.method == 'GET':
+        return render_template('jobPostingHTML.html')
+    elif request.method == 'POST':
+        companyName = request.form['companyName']
+        positionName = request.form['positionName']
+        location = request.form['location']
+        workMode = request.form['workMode']
+        salary = request.form['salary']
+        contactFName = request.form['contactFName']
+        contactLName = request.form['contactLName']
+        contactEmail = request.form['contactEmail']
+        description = request.form['description']
+    
+    # Need to fetch posting ID to continue <--------------
 
-
+@app.route('/jobDashboard')
+def jobDashboard():
+     # connection to the database module
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    jobPostings = c.execute("SELECT * FROM JobPostings").fetchall()
+    print(jobPostings)
+    return render_template("jobDashboardHTML.html", jobPostings = jobPostings)
 
 if(__name__ == '__main__'):
     app.run(debug=True)

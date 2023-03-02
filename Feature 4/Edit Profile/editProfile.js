da1ec1b61970765a77252a7e0afc5700aa340f6f
Suha <88583460+suha-ab@users.noreply.github.com>
2023-03-01 21:43:55 -0500

    edit profile html and js

---
+++ Feature 4/Edit Profile/editProfile.js
@@ -0,0 +1,8 @@
+// Javascript file for adding new work experience to profile
+var addExp = document.getElementById("addExp")
+addExp.addEventListener("click", addNewExp)
+
+function addNewExp(){
+    expContainer = document.getElementById("")
+
+}

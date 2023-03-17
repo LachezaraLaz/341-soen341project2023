4f50f1727b6cba50e579ea8dfa7d1ca9d6d0b077
LachezaraLaz <lachezara.laz@gmail.com>
2023-03-17 13:34:59 -0400

    created notif blueprint

---
+++ Implico/notifcation.py
@@ -0,0 +1,10 @@
+import sqlite3
+from flask import Flask, request, render_template, Blueprint, redirect
+#initializing Blueprint
+notification = Blueprint('notification', __name__)
+
+
+@notification.route("/notifcation.html",methods = ['GET'])
+def notif():
+    if request.method == 'GET':
+        return "hello" 
